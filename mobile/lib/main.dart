217f20efb029e002fa738698feb04f964ebfc2fb
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-09 22:50:09 +0300

    feat: launch storefront v6

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'design_system/walka_theme.dart';
-import 'features/storefront/storefront_shell_v5.dart';
+import 'features/storefront/storefront_shell_v6.dart';
 
 void main() {
   runApp(const WalkaApp());
@@ -16,7 +16,7 @@ class WalkaApp extends StatelessWidget {
       title: 'WALKA',
       debugShowCheckedModeBanner: false,
       theme: buildWalkaTheme(),
-      home: const WalkaStorefrontSplashV5(),
+      home: const WalkaStorefrontSplashV6(),
     );
   }
 }
