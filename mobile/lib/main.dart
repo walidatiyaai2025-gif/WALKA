b135ba8789c2fe91daf57e579cc731de7b307e79
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-09 21:20:20 +0300

    feat: wire WALKA application entry point

---
+++ mobile/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'design_system/walka_theme.dart';
+import 'screens/walka_screens.dart';
+
+void main() {
+  runApp(const WalkaApp());
+}
+
+class WalkaApp extends StatelessWidget {
+  const WalkaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WALKA',
+      debugShowCheckedModeBanner: false,
+      theme: buildWalkaTheme(),
+      home: const SplashScreen(),
+    );
+  }
+}
