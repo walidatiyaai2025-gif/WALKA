be8a54ce671594c0cdd6c0f7a0acb0693aa6e174
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-09 23:00:34 +0300

    test: cover all Lunch Box Amazon destinations

--- mobile/test/amazon_purchase_test.dart
+++ mobile/test/amazon_purchase_test.dart
@@ -28,4 +28,33 @@ void main() {
       );
     });
   });
+
+  group('Amazon lunch box routing', () {
+    test('blue variant maps to the official listing', () {
+      final Uri uri = amazonLunchBoxUri(WalkaAmazonLunchVariant.blue);
+      expect(uri.scheme, 'https');
+      expect(uri.host, 'www.amazon.com');
+      expect(uri.path, '/dp/$walkaLunchBoxBlueAsin');
+      expect(walkaLunchBoxBlueAsin, 'B0FQN4L8MW');
+    });
+
+    test('pink variant maps to the official listing', () {
+      final Uri uri = amazonLunchBoxUri(WalkaAmazonLunchVariant.pink);
+      expect(uri.path, '/dp/$walkaLunchBoxPinkAsin');
+      expect(walkaLunchBoxPinkAsin, 'B0FQN3W4SF');
+    });
+
+    test('green variant maps to the official listing', () {
+      final Uri uri = amazonLunchBoxUri(WalkaAmazonLunchVariant.green);
+      expect(uri.path, '/dp/$walkaLunchBoxGreenAsin');
+      expect(walkaLunchBoxGreenAsin, 'B0GPZNKF9F');
+    });
+
+    test('all lunch listings stay distinct', () {
+      final Set<Uri> uris = WalkaAmazonLunchVariant.values
+          .map(amazonLunchBoxUri)
+          .toSet();
+      expect(uris.length, 3);
+    });
+  });
 }
