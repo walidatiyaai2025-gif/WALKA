38e15132856f551f15cc31faaebc6da9b83854b6
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-10 19:02:03 +0300

    fix(FAV-002): preserve released favorites header chrome

--- mobile/lib/features/storefront/presentation/widgets/favorites/walka_favorites_header.dart
+++ mobile/lib/features/storefront/presentation/widgets/favorites/walka_favorites_header.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../design_system/components/chrome/walka_reference_top_bar.dart';
+import '../../../../../design_system/walka_theme.dart';
 
 class WalkaFavoritesHeader extends StatelessWidget {
   const WalkaFavoritesHeader({super.key});
@@ -8,9 +9,9 @@ class WalkaFavoritesHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const WalkaReferenceTopBar(
-      headerKey: ValueKey<String>('reference-favorites-header'),
-      leadingIcon: Icons.menu_rounded,
-      trailingIcon: Icons.favorite_border_rounded,
+      headerKey: ValueKey<String>('reference-favorites-topbar'),
+      trailingIcon: Icons.favorite_rounded,
+      trailingColor: WalkaColors.gold,
     );
   }
 }
