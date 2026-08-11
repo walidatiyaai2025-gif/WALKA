3b3febc71f68d9b07e7be581cab492ba90ee8180
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-11 17:24:31 +0300

    QMEDIA-093: reconcile Search visual regression with quarantine fallback

--- mobile/test/design_003_discovery_premium_test.dart
+++ mobile/test/design_003_discovery_premium_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:walka/design_system/walka_product_visual.dart';
 import 'package:walka/design_system/walka_theme.dart';
 import 'package:walka/features/catalog/catalog_state.dart';
 import 'package:walka/features/storefront/discovery_reference_v123.dart';
@@ -158,7 +159,8 @@ void main() {
         find.byKey(const ValueKey<String>('discovery-search-lunch-box:blue')),
         findsNothing,
       );
-      expect(find.byType(Image), findsOneWidget);
+      expect(find.byType(WalkaProductVisual), findsOneWidget);
+      expect(find.byType(Image), findsNothing);
       expect(tester.takeException(), isNull);
     },
   );
