69fc541653709ee93bb7f9ad2950d6494c95a618
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-11 12:12:24 +0300

    ADMIN-001: configure dashboard credentials

--- backend/config/walka.php
+++ backend/config/walka.php
@@ -6,4 +6,6 @@ return [
     'api_version' => 'v1',
     'purchase_mode' => 'amazon_redirect',
     'admin_token' => env('WALKA_ADMIN_TOKEN'),
+    'dashboard_username' => env('WALKA_ADMIN_DASHBOARD_USERNAME', 'admin'),
+    'dashboard_password' => env('WALKA_ADMIN_DASHBOARD_PASSWORD'),
 ];
