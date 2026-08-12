04b9e46c7d475539e128721c8716dfe13c1c6238
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-12 21:09:23 +0300

    PINK-QA: quarantine rejected Pink runtime candidate

--- mobile/lib/design_system/components/media/walka_product_media_admission.dart
+++ mobile/lib/design_system/components/media/walka_product_media_admission.dart
@@ -79,9 +79,9 @@ abstract final class WalkaProductMediaAdmissionRegistry {
     'lunch-box:pink': WalkaProductMediaAdmissionEntry(
       variantId: 'lunch-box:pink',
       canonicalPath: 'assets/products/lunch/pink.png',
-      state: WalkaProductMediaAdmissionState.admitted,
+      state: WalkaProductMediaAdmissionState.pending,
       sourceApproved: true,
-      canonicalExportPresent: true,
+      canonicalExportPresent: false,
     ),
     'lunch-box:green': WalkaProductMediaAdmissionEntry(
       variantId: 'lunch-box:green',
