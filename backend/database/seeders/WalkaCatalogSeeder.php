b875f013fceb7e3d6bafa54a7914c2226c38f24a
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-13 22:54:04 +0300

    CMS-011: initialize presentation order on first seed only

--- backend/database/seeders/WalkaCatalogSeeder.php
+++ backend/database/seeders/WalkaCatalogSeeder.php
@@ -19,7 +19,7 @@ final class WalkaCatalogSeeder extends Seeder
                 $productIds[] = $productData['id'];
                 $existingProduct = Product::query()->find($productData['id']);
 
-                Product::query()->updateOrCreate(
+                $product = Product::query()->updateOrCreate(
                     ['id' => $productData['id']],
                     [
                         'name' => $existingProduct?->name ?? $productData['name'],
@@ -30,12 +30,20 @@ final class WalkaCatalogSeeder extends Seeder
                     ],
                 );
 
+                if ($existingProduct === null) {
+                    $product->forceFill([
+                        'is_visible' => true,
+                        'is_featured' => false,
+                        'presentation_order' => $productOrder,
+                    ])->save();
+                }
+
                 $variantIds = [];
                 foreach ($productData['variants'] as $variantOrder => $variantData) {
                     $variantIds[] = $variantData['id'];
                     $existingVariant = ProductVariant::query()->find($variantData['id']);
 
-                    ProductVariant::query()->updateOrCreate(
+                    $variant = ProductVariant::query()->updateOrCreate(
                         ['id' => $variantData['id']],
                         [
                             'product_id' => $productData['id'],
@@ -45,6 +53,13 @@ final class WalkaCatalogSeeder extends Seeder
                             'sort_order' => $variantOrder,
                         ],
                     );
+
+                    if ($existingVariant === null) {
+                        $variant->forceFill([
+                            'is_visible' => true,
+                            'presentation_order' => $variantOrder,
+                        ])->save();
+                    }
                 }
 
                 ProductVariant::query()
