5c6364aba2e51f18a1ffa75e472f64857ffbde7c
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-13 17:42:38 +0300

    CMS-035: use remote-aware PDP gallery count

--- mobile/lib/features/products/presentation/widgets/walka_pdp_gallery.dart
+++ mobile/lib/features/products/presentation/widgets/walka_pdp_gallery.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../../design_system/walka_product_visual.dart';
 import 'walka_pdp_gallery_indicator.dart';
-import 'walka_pdp_gallery_viewport.dart';
+import 'walka_remote_pdp_gallery_viewport.dart';
 
 class WalkaPdpGallery extends StatefulWidget {
   const WalkaPdpGallery({
@@ -39,11 +39,14 @@ class _WalkaPdpGalleryState extends State<WalkaPdpGallery> {
   @override
   Widget build(BuildContext context) {
     final bool reduceMotion = MediaQuery.disableAnimationsOf(context);
+    final int itemCount =
+        WalkaRemotePdpGalleryViewport.itemCountFor(context, widget.variantId);
+    final int selectedIndex = _index.clamp(0, itemCount - 1);
     return Column(
       children: <Widget>[
-        WalkaPdpGalleryViewport(
+        WalkaRemotePdpGalleryViewport(
           controller: _controller,
-          selectedIndex: _index,
+          selectedIndex: selectedIndex,
           variantId: widget.variantId,
           kind: widget.kind,
           primaryColor: widget.primaryColor,
@@ -54,8 +57,8 @@ class _WalkaPdpGalleryState extends State<WalkaPdpGallery> {
         ),
         const SizedBox(height: 6),
         WalkaPdpGalleryIndicator(
-          selectedIndex: _index,
-          itemCount: WalkaPdpGalleryViewport.pageCount,
+          selectedIndex: selectedIndex,
+          itemCount: itemCount,
           onSelected: (int value) {
             _controller.animateToPage(
               value,
