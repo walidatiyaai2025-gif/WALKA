3e2047e4fd93b2b10edc2e9b31cf53b7cfa06470
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-13 18:21:29 +0300

    CMS-004: bind compiled capabilities to dashboard roles

--- backend/app/Enums/DashboardRole.php
+++ backend/app/Enums/DashboardRole.php
@@ -8,4 +8,43 @@ enum DashboardRole: string
     case ContentEditor = 'content_editor';
     case MediaEditor = 'media_editor';
     case Viewer = 'viewer';
+
+    /** @return list<DashboardCapability> */
+    public function capabilities(): array
+    {
+        return match ($this) {
+            self::Owner => DashboardCapability::cases(),
+            self::ContentEditor => [
+                DashboardCapability::DashboardView,
+                DashboardCapability::CatalogView,
+                DashboardCapability::ContentView,
+                DashboardCapability::ContentWrite,
+                DashboardCapability::ContentPublish,
+                DashboardCapability::ContentRestore,
+                DashboardCapability::MediaView,
+                DashboardCapability::AuditsView,
+            ],
+            self::MediaEditor => [
+                DashboardCapability::DashboardView,
+                DashboardCapability::CatalogView,
+                DashboardCapability::MediaView,
+                DashboardCapability::MediaUpload,
+                DashboardCapability::MediaAssign,
+                DashboardCapability::MediaReplace,
+                DashboardCapability::AuditsView,
+            ],
+            self::Viewer => [
+                DashboardCapability::DashboardView,
+                DashboardCapability::CatalogView,
+                DashboardCapability::ContentView,
+                DashboardCapability::MediaView,
+                DashboardCapability::AuditsView,
+            ],
+        };
+    }
+
+    public function allows(DashboardCapability $capability): bool
+    {
+        return in_array($capability, $this->capabilities(), true);
+    }
 }
