d73a8118e83496f12cfb70cda92309a1ada6f951
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-14 12:14:03 +0300

    fix: remove ineffective global JsonException import

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -10,7 +10,6 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Schedule;
-use JsonException;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
